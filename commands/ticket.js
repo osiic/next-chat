9577c0513a4f62f35ce83c52e96ed7c70cf7d297
Osi ic <osiic.offcl@gmail.com>
2023-09-19 21:49:10 +0700

    reset ping, gak jelas

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,12 +1,9 @@
 const {
   SlashCommandBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  StringSelectMenuBuilder,
-  StringSelectMenuOptionBuilder,
-  ActionRowBuilder,
+  ChannelType,
   PermissionFlagsBits,
 } = require("discord.js");
+const wait = require("node:timers/promises").setTimeout;
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,62 +12,40 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   async execute(interaction) {
-    const create = new ButtonBuilder()
-      .setCustomId("create")
-      .setLabel("Create Ticket")
-      .setStyle(ButtonStyle.Success);
-    const preview = new ButtonBuilder()
-      .setCustomId("preview")
-      .setLabel("Preview Ticket")
-      .setStyle(ButtonStyle.Success);
-    const done = new ButtonBuilder()
-      .setCustomId("done")
-      .setLabel("Save Ticket")
-      .setStyle(ButtonStyle.Success);
-
-    const select = new StringSelectMenuBuilder()
-      .setCustomId("select")
-      .setPlaceholder("Customize The Ticket")
-      .addOptions(
-        new StringSelectMenuOptionBuilder()
-          .setLabel("Custom Embed")
-          .setDescription("Edit the embedding to customize it")
-          .setValue("embed"),
-        new StringSelectMenuOptionBuilder()
-          .setLabel("Ticket Option")
-          .setDescription("Select want to use button or select menu")
-          .setValue("option"),
-      );
-
-    const row = [
-      new ActionRowBuilder().addComponents(select),
-      new ActionRowBuilder().addComponents(preview, done, create),
-    ];
-
-    const response = await interaction.reply({
-      components: [...row],
-      ephemeral: true,
-    });
+    let channel;
+    await interaction
+      .deferReply
+      // { ephemeral: true }
+      ();
 
     try {
-      const confirmation = await response.awaitMessageComponent({
-        time: 60000,
+      channel = await interaction.guild.channels.create({
+        name: `t-${interaction.user.username}`,
+        type: ChannelType.GuildText,
+        permissionOverwrites: [
+          {
+            id: interaction.user.id,
+            allow: [PermissionFlagsBits.ViewChannel],
+          },
+          {
+            id: interaction.guild.roles.everyone.id,
+            deny: [PermissionFlagsBits.ViewChannel],
+          },
+        ],
       });
+      channel.send(
+        `halo <@${interaction.user.id}> pindah ke <#1003673516861816973>`,
+      );
 
-      if (confirmation.customId === "preview") {
-        await confirmation.update({
-          content: `halo`,
-        });
-      } else if (confirmation.customId === "done") {
-        await confirmation.update({
-          content: "Action cancelled",
-        });
-      }
-    } catch (e) {
-      await interaction.editReply({
-        content: "Confirmation not received within 1 minute, cancelling",
-        components: [],
-      });
+      await interaction.editReply(
+        `Ticket is ready to use, move on <#${channel.id}>`,
+      );
+      await wait(1000 * 60);
+      channel.delete();
+    } catch (err) {
+      await interaction.editReply(
+        `Tickets cannot be created \`\`\`\n${err}\`\`\``,
+      );
     }
   },
 };
