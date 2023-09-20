982fcb9de0878506b765c8860984ace844126f1a
Osi ic <osiic.offcl@gmail.com>
2023-09-20 19:59:15 +0700

    feat(send channel): button blm bisa di click

--- commands/ticket.js
+++ commands/ticket.js
@@ -1,6 +1,10 @@
 const {
-  SlashCommandBuilder,
   ChannelType,
+  ButtonStyle,
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  SlashCommandBuilder,
   PermissionFlagsBits,
 } = require("discord.js");
 const wait = require("node:timers/promises").setTimeout;
@@ -12,6 +16,7 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   async execute(interaction) {
+    console.log(interaction);
     let channel;
     await interaction
       .deferReply
@@ -19,28 +24,47 @@ module.exports = {
       ();
 
     try {
+      const ticketEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle("Ticket Menu")
+        .setDescription(
+          "**Open a new Ticket using the button below this message!**",
+        )
+        .setImage(
+          "https://images-ext-2.discordapp.net/external/BXAWHfVpCjFQ9MD9wnUytVoZqZ0B0KUD6XZM0liR-Y0/https/i.imgur.com/AZlgvgi.png",
+        )
+        .setFooter({
+          text: "Do not use for joking or playing around",
+          iconURL: interaction.guild.iconURL(),
+        });
+
+      const openTicket = new ButtonBuilder()
+        .setLabel("Open a Ticket")
+        .setCustomId("open-ticket")
+        .setStyle(ButtonStyle.Primary)
+        .setEmoji("🎫");
+
       channel = await interaction.guild.channels.create({
-        name: `t-${interaction.user.username}`,
+        name: `ticket`,
         type: ChannelType.GuildText,
         permissionOverwrites: [
-          {
-            id: interaction.user.id,
-            allow: [PermissionFlagsBits.ViewChannel],
-          },
+          //   {
+          //     id: interaction.user.id,
+          //     allow: [PermissionFlagsBits.ViewChannel],
+          //   },
           {
             id: interaction.guild.roles.everyone.id,
             deny: [PermissionFlagsBits.ViewChannel],
           },
         ],
       });
-      channel.send(
-        `halo <@${interaction.user.id}> pindah ke <#1003673516861816973>`,
-      );
 
-      await interaction.editReply(
-        `Ticket is ready to use, move on <#${channel.id}>`,
-      );
-      await wait(1000 * 60);
+      channel.send({
+        embeds: [ticketEmbed],
+        components: [new ActionRowBuilder().addComponents(openTicket)],
+      });
+      await interaction.editReply(`Ticket is ready, move on <#${channel.id}>`);
+      await wait(1000 * 6);
       channel.delete();
     } catch (err) {
       await interaction.editReply(
